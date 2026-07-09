f45a8eb1c30451c15ab098004519d8185e8baebf
Anushka-Naik28 <anushkanaik285@gmail.com>
2026-07-09 22:34:19 +0530

    fix: allow X-DMail-Email/Password custom headers in CORS options

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = 8765
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-DMail-Email, X-DMail-Password, x-dmail-email, x-dmail-password")
   if (req.method === "OPTIONS") return res.sendStatus(200)
   next()
 })
